c22900bd69effbb47d134bdb8b5115c0e69f5f97
PascalDragos <dragos-costin.pascal@student.tuiasi.ro>
2020-05-09 18:52:52 +0300

    Fix display

--- python/buzz.py
+++ python/buzz.py
@@ -25,8 +25,9 @@ def initialize_GPIO():
 
 
 def print_segment(char):
-    zero = [0,0,0,0,0,0,0]
-    one = [1,1,1,1,1,1,0]
+    null = [0,0,0,0,0,0,0]
+    zero = [1,1,1,1,1,1,0]
+    one = [0,1,1,0,0,0,0]
     two = [1,1,0,1,1,0,1]
     three = [1,1,1,1,0,0,1]
     four = [0,1,1,0,0,1,1]
@@ -77,10 +78,12 @@ def print_segment(char):
             GPIO.output(segments[i], zero[i])        
 
 def print_frequency(frequency):
-    d0 = frequency % 10
-    d1 = frequency / 10 % 10
-    d2 = frequency / 100 % 10
-    d3 = frequency / 1000
+    freq = int(frequency)
+    d3 = freq % 10
+    d2 = (freq // 10) % 10
+    d1 = (freq // 100) % 10
+    d0 = freq // 1000
+    #print(f"{d3} {d2} {d1} {d0}")
     print_digit(0, d0)
     print_digit(1, d1)
     print_digit(2, d2)
@@ -88,26 +91,42 @@ def print_frequency(frequency):
 
 
 def print_digit(digit, number):
-    delay_time = 0.001
+    delay_time = 0.0007
+    
+    print_segment(number)
     GPIO.output(DS[digit], GPIO.LOW)
+    #print_segment(number)
+    '''
+    zero = [1,1,1,1,1,1,0]
+    
+    for i in range(7):
+        GPIO.output(segments[i], zero[i])        
+    '''
+
     time.sleep(delay_time)
     GPIO.output(DS[digit], GPIO.HIGH)
 
 
 def main():
+    
     pi = pigpio.pi()
-    buzzer1 = 14
-
+    buzzer1 = 18
+    #file = open("shared_memory.txt", "w")
     try:
         initialize_GPIO()
+      #  print_digit(2,1)
         while(True):
+            
+            
             file = open("shared_memory.txt", "r")
             frequency = file.read()
+            #print_digit(3,3)
             
+            #print_frequency(6789)
             if frequency:
                 frequency = int(frequency)
                 pi.hardware_PWM(buzzer1, frequency, 500000)
-
+                print_frequency(frequency)
             file.close()
             
     except KeyboardInterrupt:
@@ -119,3 +138,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
