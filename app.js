a4f2132719fd52b297fa8f0a903a312d2f4fba02
PascalDragos <dragos-costin.pascal@student.tuiasi.ro>
2020-05-11 00:50:02 +0300

    FromWindowsToLinux

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ app.get('/stats', (req, res) => {
 	// let freeSpacePrecentage  = execSync("df -H --output=pcent,source | grep root").toString();
 	// let avaibleSpace  = execSync("df --output=avail,source | grep root").toString();
 	let df = execSync("df | grep root").toString();
-	console.log(df);
 	temp = temp.split("=")[1].split("'")[0];
 	frq = frq.split("=")[1]/1000000;
 	volts = volts.split("=")[1].substr(0,3);
